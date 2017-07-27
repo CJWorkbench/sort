018ee789b00e6c616676ea2ba0a6946bbe771924
alexgonca <alexandre.reportagem@gmail.com>
2017-07-26 19:49:15 -0700

    initialize repo

---
+++ sortbynumericcolumn.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+class Importable:
+    @staticmethod
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def event():
+        pass
+
+    @staticmethod
+    def render(wf_module, table):
+        sortby = wf_module.get_param_string('sortby')
+        order = wf_module.get_param_menu_string('order')
+
+        if sortby == '':
+            wf_module.set_ready(notify=False)
+            return None
+        elif sortby not in table.columns:
+            wf_module.set_error('Invalid column.')
+            return None
+        else:
+            if table[sortby].dtype != np.float64 and table[sortby].dtype != np.int64:
+                table[sortby] = table[sortby].str.replace(',', '')
+                table[sortby] = table[sortby].astype(float)
+            if order == 'Ascending':
+                newtab = table.sort_values(sortby)
+            else:
+                newtab = table.sort_values(sortby, ascending = False)
+            return newtab
