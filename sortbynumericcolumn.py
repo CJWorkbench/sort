ea5803205652aa5a2018e893c95903a6a5c71c4b
Jonathan Stray <jonathanstray@gmail.com>
2017-11-24 13:00:42 -0800

    Menu params use index not string

--- sortbynumericcolumn.py
+++ sortbynumericcolumn.py
@@ -8,7 +8,7 @@ def render(table, params):
         if table[sortby].dtype != np.float64 and table[sortby].dtype != np.int64:
             table[sortby] = table[sortby].str.replace(',', '')
             table[sortby] = table[sortby].astype(float)
-        if order == 'Ascending':
+        if order == 0:  # ascending
             newtab = table.sort_values(sortby)
         else:
             newtab = table.sort_values(sortby, ascending = False)
