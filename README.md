26d69c3c84005a2e97e490b7e7e94dd35de8a483
Georgios Pitsiladis <g.v.pitsiladis@gmail.com>
2020-02-18 16:42:49 +0200

    Replace with new version
    This commit does a lot of things
    
    - Removes the old deprecated code of the module
      and replaces it with the one in cjworkbench sort internal module
    - Internationalizes the module
    - Adds a new README, a .giignore, a and setup.py

--- README.md
+++
@@ -1,2 +0,0 @@
-# sort
-Numerically or alphabetically.
---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name="sort",
+    version="0.0.1",
+    description="Sort rows by selected columns' values",
+    author="Adam Hooper",
+    author_email="adam@adamhooper.com",
+    url="https://github.com/CJWorkbench/sort",
+    packages=[""],
+    py_modules=["sort"],
+    install_requires=["pandas==0.25.0"],
+)
